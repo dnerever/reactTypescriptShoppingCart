8a6bcbbb15841e64581b720086a709047305717a
Keith Bates <bateskeith@pm.me>
2024-06-23 17:28:30 -0600

    working through error before with rendering

---
+++ shopping-cart/src/components/CurrencyFormatter/CurrencyFormatter.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from 'react'
+import classes from './currency-formatter.module.scss'
+
+interface Props {
+    amount: number
+}
+
+export const CurrencyFormatter: FunctionComponent<Props> = ({ amount }) => {
+    const formattedAmount = amount.toLocaleString('en-GB', {
+        style: 'currency',
+        currency: 'USD'
+    })
+
+    return <span className={classes.currency}>{formattedAmount}</span>
+}
--- shopping-cart/src/components/Products/Products.tsx
+++ shopping-cart/src/components/Products/Products.tsx
@@ -25,61 +25,61 @@ export const Products: FunctionComponent = () => {
     const [products, setProducts] = useState<Product[]>([])
     const [error, setError] = useState(false)
     const [cart, setCart] = useLocalStorageState<CartProps>('cart', {})
-}
+    
+    useEffect(() => {
+        fetchData(API_URL)
+    }, [])
 
-useEffect(() => {
-    fetchData(API_URL)
-}, [])
-
-async function fetchData(url: string) {
-    try {
-        const response = await fetch(url)
-        if (response.ok) {
-            const data = await response.json()
-            setProducts(data.products)
-            setIsLoading(false)
-        } else {
+    async function fetchData(url: string) {
+        try {
+            const response = await fetch(url)
+            if (response.ok) {
+                const data = await response.json()
+                setProducts(data.products)
+                setIsLoading(false)
+            } else {
+                setError(true)
+                setIsLoading(false)
+            }
+        } catch (error) {
             setError(true)
             setIsLoading(false)
         }
-    } catch (error) {
-        setError(true)
-        setIsLoading(false)
     }
+
+    const addToCart = (product: Product):void => {
+        product.quantity = 1
+        
+        setCart((prevCart: any) => ({
+            ...prevCart,
+            [product.id]: product,
+        }))
+    }
+
+    const isInCart = (productId: number): boolean => Object.keys(cart || {}).includes(productId.toString())
+
+    if (error) {
+        return <h3 className={classes.error}>An error occurred when fetching data. Please check the API and try again.</h3>
+    }
+
+    if (isLoading) {
+        return <Loader />
+    }
+
+    return (
+        <section className={classes.productPage}>
+            <h1>Products</h1>
+
+            <div className={classes.container}>
+                {products.map(product => (
+                    <div className={classes.product} key={product.id}>
+                        <img src={product.thumbnail} alt={product.title} />
+                        <h3>{product.title}</h3>
+                        <p>Price: <CurrencyFormatter amount={product.price} /></p>
+                        <button disabled={isInCart(product.id)} onClick={() => addToCart(product)}>Add to Cart</button>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
-
-const addToCart = (product: Product):void => {
-    product.quantity = 1
-
-    setCart((prevCart) => ({
-        ...prevCart,
-        [product.id]: product,
-    }))
-}
-
-const isInCart = (productId: number): boolean => Object.keys(cart || {}).includes(productId.toString())
-
-if (error) {
-    return <h3 className={classes.error}>An error occurred when fetching data. Please check the API and try again.</h3>
-}
-
-if (isLoading) {
-    return <Loader />
-}
-
-return (
-    <section className={classes.productPage}>
-        <h1>Products</h1>
-
-        <div className={classes.container}>
-            {Products.map(product => (
-                <div className={classes.product} key={product.id}>
-                    <img src={product.thumbnail} alt={product.title} />
-                    <h3>{product.title}</h3>
-                    <p>Price: <CurrencyFormatter amount={product.price} /></p>
-                    <button disabled={isInCart(product.id)} onClick={() => addToCart(product)}>Add to Cart</button>
-                </div>
-            ))}
-        </div>
-    </section>
-)
